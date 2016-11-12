2fe9e6fb5b13d9762adef47a464288b85c480cd5
asimonia <alex.simonian@gmail.com>
2016-11-12 17:18:49 -0600

    Added date randomizer

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 "use strict";
 
 
-var url = "https://api.nasa.gov/planetary/apod?api_key=oGVn37OPC6t8IQ5KJVJPTHKPGu0ti67EQXOxKEWd&date=2010-01-10&hd=True";
+// returns a random date from two arguments; insert into the url to get a random picture for slideshow
+function randomDate(start, end) {
+    var d = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
 
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
 
+    return [year, month, day].join('-');
+}
+
+var nasa_date = randomDate(new Date(2005, 6, 16), new Date());
+var url = `https://api.nasa.gov/planetary/apod?api_key=oGVn37OPC6t8IQ5KJVJPTHKPGu0ti67EQXOxKEWd&date=${nasa_date}&hd=True`;
+
+// calls the NASA API to transform the picture on the DOM
 $.ajax({
 	  url: url,
 	  success: function(result) {
@@ -11,5 +25,7 @@ $.ajax({
 	  if (result.media_type === "image") {
 	  	$("#apod_img_id").attr("src", result.url);
 	  }
+
+	  $("#apod_title").text(result.title);
 	}
 });
