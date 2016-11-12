dc0fe9cdd6b69023484cf028f28aaee8f9ab1452
asimonia <alex.simonian@gmail.com>
2016-11-12 17:44:52 -0600

    Fade pics

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 "use strict";
 
 
-// returns a random date from two arguments; insert into the url to get a random picture for slideshow
 function randomDate(start, end) {
     var d = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())),
         month = '' + (d.getMonth() + 1),
@@ -14,18 +13,33 @@ function randomDate(start, end) {
     return [year, month, day].join('-');
 }
 
-var nasa_date = randomDate(new Date(2005, 6, 16), new Date());
-var url = `https://api.nasa.gov/planetary/apod?api_key=oGVn37OPC6t8IQ5KJVJPTHKPGu0ti67EQXOxKEWd&date=${nasa_date}&hd=True`;
+var displayPic = function () {
 
-// calls the NASA API to transform the picture on the DOM
-$.ajax({
-	  url: url,
-	  success: function(result) {
+	let nasa_date = randomDate(new Date(2005, 6, 16), new Date());
+	let url = `https://api.nasa.gov/planetary/apod?api_key=oGVn37OPC6t8IQ5KJVJPTHKPGu0ti67EQXOxKEWd&date=${nasa_date}&hd=True`;
 
-	  if (result.media_type === "image") {
-	  	$("#apod_img_id").attr("src", result.url);
-	  }
+	$.ajax({
+		url: url,
+		success: function(result) {
 
-	  $("#apod_title").text(result.title);
-	}
-});
+			let nasa_date = randomDate(new Date(2005, 6, 16), new Date());
+			let url = `https://api.nasa.gov/planetary/apod?api_key=oGVn37OPC6t8IQ5KJVJPTHKPGu0ti67EQXOxKEWd&date=${nasa_date}&hd=True`;
+
+	  	    if (result.media_type !== "image") {
+		    	console.log("No luck");
+		    } else {
+		  	    $("#apod_img_id").hide().empty();
+		  		$("#apod_title").hide().empty();
+		  		$("#apod_img_id").attr("src", result.url).fadeIn();
+		  		$("#apod_title").text(result.title).fadeIn();
+		    }
+
+		    setTimeout(function () {
+		  		displayPic();
+		  	}, 3000);	  
+		}
+	});
+
+};
+
+$(document).ready(displayPic);
